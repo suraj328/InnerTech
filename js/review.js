cb3b94bf85d7ff8735d7a714362f6bc98eb5ffa7
suraj sah <70622673+suraj328@users.noreply.github.com>
2023-01-04 22:03:00 +0545

    web design completed

--- js/review.js
+++ js/review.js
@@ -1,3 +1,32 @@
 $(document).ready(()=>{
-    
+    $('body').scrollspy({ target: '#navbar-example' });
+    $('#myRange').change(()=>{
+        var starnum =$('#myRange').val(); 
+        
+        switch (parseInt(starnum)) {
+            case 1:
+                $('#fillstar').html('');
+                $('#fillstar').html('<i class="fa fa-star text-warning">');
+                break;
+            case 2:
+                $('#fillstar').html('');
+                $('#fillstar').html('<i class="fa fa-star text-warning"><i class="fa fa-star text-warning">');
+                break;
+            case 3:
+                $('#fillstar').html('');
+                $('#fillstar').html('<i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning">');
+                break; 
+            case 4:
+                $('#fillstar').html('');
+                $('#fillstar').html('<i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning">');
+                break;
+            case 5:
+                $('#fillstar').html('');
+                $('#fillstar').html('<i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning"><i class="fa fa-star text-warning">');
+                break; 
+            default:
+                break;
+        } 
+    });
+
 });
